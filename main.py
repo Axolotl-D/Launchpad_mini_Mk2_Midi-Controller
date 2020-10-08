843c3a733521d2511bfab59d692b6fc32053425b
Axolotl-D <alonzohd031@gmail.com>
2020-10-09 01:19:15 +0200

    Mute Button now Works, Light support not available

--- main.py
+++ main.py
@@ -12,31 +12,63 @@ import time
 
 import rtmidi
 from pynput.keyboard import Controller, Key
-from rtmidi.midiutil import open_midiinput
+from rtmidi.midiutil import open_midiinput, open_midioutput
 
 log = logging.getLogger('midiin_callback')
 logging.basicConfig(level=logging.DEBUG)
 keyboard = Controller()
 
+# global lastAction
+# lastAction = True
 
 class MidiInputHandler(object):
+
+    lastAction = True
+
+
     def __init__(self, port):
         self.port = port
         self._wallclock = time.time()
 
-    def __call__(self, event, data=None):
+    def __call__(self, event, data=None, ):
         message, deltatime = event
+        # midiout = rtmidi.MidiOut()
+        # available_ports = midiout.get_ports()
+        # # print(available_ports)
+        # midiout.open_port(port)
+        # # if available_ports:
+        # #     midiout.open_port(1)
+        # # else:
+        # #     midiout.open_virtual_port("My virtual output")
         self._wallclock += deltatime
         print("[%s] @%0.6f %r" % (self.port, self._wallclock, message))
+
+        self.midimethod(message, midiout)
+
+    def midimethod(self, message, midiout):
         if message[1] == 0:
-            if message[2] != 0:
+            if message[2] == 0:
+
+                print("Done")
+
+            elif message[2] != 0:
                 print("Taste 0")
                 keyboard.press(Key.f13)
                 keyboard.release(Key.f13)
-            #     144,0,15
+                #     True,0,15
+                with midiout:
+                    if MidiInputHandler.lastAction == True:
+                        # midiout.send_message([176, 0, 0])
+                        midiout.send_message([0x90, 0, 12])
+                        midiout.close_port
+                        print("Lights on")
+                        MidiInputHandler.lastAction = not MidiInputHandler.lastAction
+                    else:
+                        midiout.send_message([0x90, 0, 12])
+                        print("Lights off")
+                        MidiInputHandler.lastAction = not MidiInputHandler.lastAction
+                        midiout.close_port
 
-            else:
-                print("Done")
 
 
 # Prompts user for MIDI input port, unless a valid port number or name
@@ -46,12 +78,13 @@ port = sys.argv[1] if len(sys.argv) > 1 else None
 
 try:
     midiin, port_name = open_midiinput(port)
+    midiout, port_name = open_midioutput(port)
 except (EOFError, KeyboardInterrupt):
     sys.exit()
-
+lastAction = True
 print("Attaching MIDI input callback handler.")
-midiin.set_callback(MidiInputHandler(port_name))
 
+midiin.set_callback(MidiInputHandler(port_name))
 print("Entering main loop. Press Control-C to exit.")
 try:
     # Just wait for keyboard interrupt,
@@ -63,4 +96,5 @@ except KeyboardInterrupt:
 finally:
     print("Exit.")
     midiin.close_port()
+    midiout.close_port()
     del midiin
--- test.py
+++ test.py
@@ -1,3 +1,104 @@
-keyboard = Controller()
-keyboard.press('a')
-keyboard.release('a')
+# import time
+# import rtmidi
+#
+# midiout = rtmidi.MidiOut()
+# available_ports = midiout.get_ports()
+#
+# if available_ports:
+#     midiout.open_port(1)
+# else:
+#     midiout.open_virtual_port("My virtual output")
+#
+# with midiout:
+#     note_c = [144, 60, 112] # channel 1, middle C, velocity 112
+#     note_d = [144, 62, 112]
+#     note_e = [144, 64, 112]
+#     note_f = [144, 65, 112]
+#     note_g = [144, 67, 112]
+#     note_a = [144, 69, 112]
+#
+#     note_off = [0x80, 0, 0x0D]
+#     midiout.send_message(note_c)
+#     # print(note_on)
+#     time.sleep(0.5)
+#     midiout.send_message(note_d)
+#     time.sleep(0.5)
+#     midiout.send_message(note_e)
+#     time.sleep(0.5)
+#     midiout.send_message(note_f)
+#     time.sleep(0.5)
+#     midiout.send_message(note_g)
+#     time.sleep(1)
+#     midiout.send_message(note_g)
+#     time.sleep(1)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_g)
+#     time.sleep(2)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_a)
+#     time.sleep(0.5)
+#     midiout.send_message(note_g)
+#     time.sleep(2)
+#     midiout.send_message(note_f)
+#     time.sleep(0.5)
+#     midiout.send_message(note_f)
+#     time.sleep(0.5)
+#     midiout.send_message(note_f)
+#     time.sleep(0.5)
+#     midiout.send_message(note_f)
+#     time.sleep(0.5)
+#     midiout.send_message(note_e)
+#     time.sleep(1)
+#     midiout.send_message(note_e)
+#     time.sleep(1)
+#     midiout.send_message(note_g)
+#     time.sleep(0.5)
+#     midiout.send_message(note_g)
+#     time.sleep(0.5)
+#     midiout.send_message(note_g)
+#     time.sleep(0.5)
+#     midiout.send_message(note_g)
+#     time.sleep(0.5)
+#     midiout.send_message(note_c)
+#     time.sleep(5)
+#     # midiout.send_message(note_off)
+#     # time.sleep(0.1)
+#
+# del midiout
+import time
+import rtmidi
+
+
+def method():
+    global midiout, available_ports, note_on, note_off
+    midiout = rtmidi.MidiOut()
+    available_ports = midiout.get_ports()
+    if available_ports:
+        midiout.open_port(0)
+    else:
+        midiout.open_virtual_port("My virtual output")
+    with midiout:
+        note_on = [0x90, 60, 112]  # channel 1, middle C, velocity 112
+        note_off = [0x80, 60, 0]
+        midiout.send_message(note_on)
+        time.sleep(0.5)
+        midiout.send_message(note_off)
+        time.sleep(0.1)
+    del midiout
+
+
+method()
+
+method()
