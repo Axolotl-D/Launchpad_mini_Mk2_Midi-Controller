6fa4d5f550224cdb8c7a935d56bda8d42e9ca835
Stevie2601 <stefan.geiselhart@t-online.de>
2020-10-11 16:44:22 +0200

    added blink support
    Signed-off-by: Axolotl-D <alonzohd031@gmail.com>

--- lightTest.py
+++ lightTest.py
@@ -1,3 +1,6 @@
+import time
+
+
 def sendLight(key, color, midiout):
     colordict = {
         "red": 79,
@@ -25,3 +28,39 @@ def sendLight2(key, color, midiout):
     midiout.send_message([176, key, colordict[color]])
 
     print("Color done")
+
+def blinkLight(key, color1, color2, times, midiout):
+    colordict = {
+        "red": 79,
+        "green": 100,
+        "off": 0,
+        "yellow": 127,
+        "amber": 50,
+        "dark red": 10
+    }
+
+    for x in range(times):
+        midiout.send_message([176, key, colordict[color1]])
+        time.sleep(0.5/(times/2))
+        midiout.send_message([176, key, colordict[color2]])
+        time.sleep(0.5/(times/2))
+
+    midiout.send_message([144, key, 100])
+
+def blinkLight2(key, color1, color2, times, midiout):
+    colordict = {
+        "red": 79,
+        "green": 100,
+        "off": 0,
+        "yellow": 127,
+        "amber": 50,
+        "dark red": 10
+    }
+
+    for x in range(times):
+        midiout.send_message([176, key, colordict[color1]])
+        time.sleep(0.5/(times/2))
+        midiout.send_message([176, key, colordict[color2]])
+        time.sleep(0.5/(times/2))
+
+    midiout.send_message([176, key, 100])
